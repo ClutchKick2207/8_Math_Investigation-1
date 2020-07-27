a2d34773c2e06b1c51fa68d75deddf66d35be627
Ariz Ahmed <ariz.ahmed@wellard.net.au>
2020-07-28 07:37:51 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,3 +1,10 @@
+#Error checking method for an input:
+
+while x != y and z != y and m != y:
+    c = input('testing')
+    
+
+
 #Closest number snippet
 myNumber = 7
 myList = [1, 5, 10, 19]
@@ -6,4 +13,11 @@ closestNumber = min(myList, key=lambda x:abs(x-myNumber))
 print(closestNumeber)
 
 
-index = mylist.index(element)
+#How to find where a list is
+mylist = [21, 5, 8, 52, 21, 87]
+item = 8
+
+#search for the item
+index = mylist.index(item)
+
+print('The index of', item, 'in the list is:', index)
